cc6b1e2aa3d164b0bf0a2c0bda4294539415a0c6
chris <christopher102994@gmail.com>
2020-01-19 19:20:56 -0500

    Update start_app.sh
    Commenting.

--- rootfs/app/start_app.sh
+++ rootfs/app/start_app.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This is the start_app script.
+# sed -i "s#APP_COMMAND#<Your App's Command>#g" /app/start_app.sh
+# The command above will ensure that the script works as intended.
+
 export HOME=/config
 
 APP_COMMAND &
